38b7de96efa3ac9208ebf7658f075e9f5c007616
Ana Paula de Abreu Batista <anapdeabreu@gmail.com>
2026-06-18 12:54:01 -0300

    Delete complex SQL query for client purchases
    Removed a complex SQL query for listing clients who made purchases in all stores.

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -1,29 +1,7 @@
 -- Script: consultas.sql
 -- Objetivo: Consultas de média/alta complexidade para o Projeto Tamar
 
-/*CONSULTA 1: DIVISÃO RELACIONAL
-Objetivo: Listar o Nome e o CPF dos clientes (Pessoas) que realizaram
-pedidos em TODAS as lojas cadastradas no sistema.
-
-Justificativa: Ideal para identificar "super apoiadores" do Projeto Tamar,
-ou seja, consumidores extremamente fiéis que viajam e consomem produtos 
-de artesãos em absolutamente todas as filiais comerciais.*/
-
-SELECT p.Nome, p.CPF
-FROM PESSOA p
-WHERE NOT EXISTS (
-    SELECT l.UF, l.cidade
-    FROM LOJA l
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM PEDIDO pd
-        WHERE pd.CPF = p.CPF
-          AND pd.UF = l.UF
-          AND pd.cidade = l.cidade
-    )
-);
-
-/*CONSULTA 2: DIVISÃO RELACIONAL 
+/*CONSULTA 1: DIVISÃO RELACIONAL 
 Objetivo: Listar o Nome e CPF dos Pesquisadores que já atuaram em 
 resgates em TODAS as Áreas de Monitoramento do projeto.
  
